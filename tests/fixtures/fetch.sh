29738fd2d1d1af07b93018d0ec7be0bf9dc35bc5
Martin Czygan <martin.czygan@gmail.com>
2020-11-24 16:06:24 +0100

    move data into subdir

---
+++ tests/fixtures/fetch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Fetches release entity mentioned in verify.csv and cache them locally.
+
+set -e -u
+set -o pipefail
+
+API="https://api.fatcat.wiki/v0"
+CSV="verify.csv"
+
+# Just release ATM, but could extend to other types.
+mkdir -p release
+
+for ident in $(awk -F, '{print $1"\n"$2}' "$CSV"); do
+    dst="release/$ident"
+	if [ -f "$dst" ]; then
+		echo >&2 "[cached] $dst"
+		continue
+	fi
+	tempfile=$(mktemp)
+	curl -sL --fail "$API/release/$ident" | jq --sort-keys . >"$tempfile" && mv "$tempfile" "$dst"
+	echo >&2 "[fetched] $dst"
+done
