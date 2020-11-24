ea41317e6765b18310c9e8ba830d75bbe758f854
Martin Czygan <martin.czygan@gmail.com>
2020-11-24 15:52:14 +0100

    log downloads

--- tests/fixtures/verify-entity-download.sh
+++ tests/fixtures/verify-entity-download.sh
@@ -10,9 +10,10 @@ CSV="verify.csv"
 
 for ident in $(awk -F, '{print $1"\n"$2}' "$CSV"); do
 	if [ -f "$ident" ]; then
-        >&2 echo "[cached] $ident"
+		echo >&2 "[cached] $ident"
 		continue
 	fi
-    tempfile=$(mktemp)
-	curl -sL --fail "$API/release/$ident" | jq --sort-keys . > "$tempfile" && mv "$tempfile" "$ident"
+	tempfile=$(mktemp)
+	curl -sL --fail "$API/release/$ident" | jq --sort-keys . >"$tempfile" && mv "$tempfile" "$ident"
+	echo >&2 "[fetched] $ident"
 done
