ee705e9adf6bde2b8eed7c2b6edae360e595cad2
Martin Czygan <martin.czygan@gmail.com>
2020-11-25 10:36:19 +0100

    fetch: check dependencies

--- tests/data/fetch.sh
+++ tests/data/fetch.sh
@@ -7,9 +7,17 @@ set -o pipefail
 
 API="https://api.fatcat.wiki/v0"
 CSV="verify.csv"
+RELEASE_DIR="release"
+
+for prog in curl jq; do
+	command -v $prog >/dev/null 2>&1 || {
+		echo >&2 "$prog required"
+		exit 1
+	}
+done
 
 # Just release ATM, but could extend to other types.
-mkdir -p release
+mkdir -p "$RELEASE_DIR"
 
 for ident in $(awk -F, '{print $1"\n"$2}' "$CSV"); do
 	dst="release/$ident"
