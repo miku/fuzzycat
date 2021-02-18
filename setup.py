19470f627cfd12bb2efee4f020bb050b57fa1b50
Martin Czygan <martin.czygan@gmail.com>
2021-02-18 23:45:19 +0100

    fix entry point

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r") as fh:
         python_requires=">=3.5",
         zip_safe=False,
         entry_points={"console_scripts": [
-            "fuzzycat=fuzzycat"
+            "fuzzycat=fuzzycat:__main__"
         ]},
         install_requires=[
             "elasticsearch",
