c587a084defe54103aa147b7ab91542a11a548b1
Martin Czygan <martin.czygan@gmail.com>
2021-09-15 20:49:13 +0200

    matching: actually return the specified number of results

--- fuzzycat/matching.py
+++ fuzzycat/matching.py
@@ -86,7 +86,7 @@ def match_release_fuzzy(
     }
     resp = es.search(body=body, index="fatcat_release")
     if es_compat_hits_total(resp) > 0:
-        return response_to_entity_list(resp, entity_type=ReleaseEntity, api=api)
+        return response_to_entity_list(resp, entity_type=ReleaseEntity, size=size, api=api)
 
     # Get fuzzy.
     # https://www.elastic.co/guide/en/elasticsearch/reference/current/common-options.html#fuzziness
@@ -104,7 +104,7 @@ def match_release_fuzzy(
     }
     resp = es.search(body=body, index="fatcat_release")
     if es_compat_hits_total(resp) > 0:
-        return response_to_entity_list(resp, entity_type=ReleaseEntity, api=api)
+        return response_to_entity_list(resp, entity_type=ReleaseEntity, size=size, api=api)
 
     # TODO: perform more queries on other fields.
     return []
